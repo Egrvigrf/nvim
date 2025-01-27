77393b9b482f89a0013a7b9a1869f6ca2a8f3fa0
Egrvigrf <2455737667@qq.com>
2025-01-28 00:04:11 +0800

    dashboard增加同步选项

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,13 +1,17 @@
 -- 获取操作系统类型
 local is_windows = vim.fn.has('win32') == 1
 local config_path
+local script_path
 
+-- 获取 init.lua 的路径
 if is_windows then
     -- Windows 系统的路径
     config_path = "$LOCALAPPDATA\\nvim\\init.lua"
+    script_path = "$LOCALAPPDATA\\nvim\\update_config.sh"  -- 脚本路径与 init.lua 同级
 else
     -- 非 Windows 系统的路径（例如 Linux）
     config_path = "~/.config/nvim/init.lua"
+    script_path = "~/.config/nvim/update_config.sh"  -- 脚本路径与 init.lua 同级
 end
 
 return {
@@ -28,16 +32,29 @@ return {
             extensions = {},
         }
 
+        -- 执行自定义的 update_config.sh 脚本
+        local function update_config()
+            -- 根据操作系统选择路径
+            if is_windows then
+                -- Windows 系统
+                vim.fn.system('bash "' .. vim.fn.expand(script_path) .. '"')
+            else
+                -- Linux 或 macOS 系统
+                vim.fn.system('bash ' .. vim.fn.expand(script_path))
+            end
+            print("配置文件已更新！")
+        end
+
         require('dashboard').setup({
             theme = "doom", -- 或 'hyper'
             config = {
                 header = {
-                "███████╗██╗      █████╗ ████████╗███████╗██████╗ ",  
-                "██╔════╝██║     ██╔══██╗╚══██╔══╝██╔════╝██╔══██╗",  
-                "█████╗  ██║     ███████║   ██║   █████╗  ██║  ██║",  
-                "██╔══╝  ██║     ██╔══██║   ██║   ██╔══╝  ██║  ██║",  
-                "███████╗███████╗██║  ██║   ██║   ███████╗██████╔╝",  
-                "╚══════╝╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═════╝  "
+                    "███████╗██╗      █████╗ ████████╗███████╗██████╗ ",
+                    "██╔════╝██║     ██╔══██╗╚══██╔══╝██╔════╝██╔══██╗",
+                    "█████╗  ██║     ███████║   ██║   █████╗  ██║  ██║",
+                    "██╔══╝  ██║     ██╔══██║   ██║   ██╔══╝  ██║  ██║",
+                    "███████╗███████╗██║  ██║   ██║   ███████╗██████╔╝",
+                    "╚══════╝╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═════╝  "
                 },
                 center = {
                     {
@@ -64,10 +81,14 @@ return {
                         key = "c",
                         action = "edit " .. config_path  -- 根据系统路径设置
                     },
+                    {
+                        icon = "⚙️ ",
+                        desc = "Update Config       ",
+                        key = "P",
+                        action = update_config  -- 执行脚本更新配置
+                    },
                 },
-                footer = {"Maomaochong",
-                            "~",
-                        }, -- 自定义脚注
+                footer = {"Maomaochong", "~"},
             },
         })
     end
