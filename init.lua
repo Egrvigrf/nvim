3102da7bef553dd2fdeb27c55a23b211a9fc6e70
Egrvigrf <2455737667@qq.com>
2025-02-09 01:58:38 +0800

    增加了noice插件

--- init.lua
+++ init.lua
@@ -415,6 +415,21 @@ require("lazy").setup({
         end,
         ft = { "markdown" },
     },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+    opts = {
+    -- add any options here
+    },
+    dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+        "MunifTanjim/nui.nvim",
+        -- OPTIONAL:
+        --   `nvim-notify` is only needed, if you want to use the notification view.
+        --   If not available, we use `mini` as the fallback
+        "rcarriga/nvim-notify",
+        }
+    },
 })
 -- 创建一个函数来更新 theme.txt 文件
 local config_dir = vim.fn.stdpath("config")
@@ -461,6 +476,24 @@ vim.api.nvim_create_autocmd("User", {
         require("lualine").setup({
             options = { theme = "auto" },
             --sections = { lualine_a = { "mode" }, lualine_c = { "filename" } }
+        })
+        require("noice").setup({
+            lsp = {
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+                 ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                },
+            },
+            -- you can enable a preset for easier configuration
+            presets = {
+                bottom_search = true, -- use a classic bottom cmdline for search
+                command_palette = true, -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = false, -- add a border to hover docs and signature help
+            },
         })
     end
 })
